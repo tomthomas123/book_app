3fef2075d988b9fec9f523023ff1702dc928eb6c
tomthomas123 <tomthomas1248@gmail.com>
2022-11-02 12:23:25 +0530

    add feauture addded

--- bookapp.py
+++ bookapp.py
@@ -1,3 +1,7 @@
+import mysql.connector
+mydb = mysql.connector.connect(host = 'localhost',user = 'root', password = '',database = 'librarydb')
+
+mycursor = mydb.cursor()
 while True:
     print("select an option from the menu")
     print("""             1. add book
@@ -10,6 +14,14 @@ while True:
     choice =int(input("Enter your option : "))
     if choice==1:
         print("book entry selected")
+        name = input("enter the name of the book :")
+        author =input("Enter the author name :  ")
+        category = input("Enter the category : ")
+        price =input("Enter the price per day : ")
+        sql = 'INSERT INTO `books`(`bookname`, `bookauthor`, `bookcategory`, `book_rentprice`) VALUES  (%s,%s,%s,%s)'
+        data = (name,author,category,price)
+        mycursor.execute(sql,data)
+        mydb.commit()
     elif choice==2:
         print("view all books selected ")
     elif choice==3:
